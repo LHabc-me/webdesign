bc36faefcf9592c5d9f9339fade718ecfc753f90
delete-cloud <1526706882@qq.com>
2023-05-26 17:10:39 +0800

    55

--- backend/src/main/java/com/web_design/backend/controller/upload/UploadFileController.java
+++ backend/src/main/java/com/web_design/backend/controller/upload/UploadFileController.java
@@ -53,78 +53,47 @@ public class UploadFileController {
         return "redirect:/index.html";
     }
 
-    @PostMapping("/multi-file-upload")
-    public String multiFileUpload(@RequestParam("up-multi-file") MultipartFile[] files) {
-        System.out.println("开始处理上传文件");
-        Map<String, Object> info = new HashMap<>();
 
-        try {
-            for (MultipartFile file : files) {
-                if (!file.isEmpty()) {
-                    info.put("上传文件的参数名称", file.getName());
-                    info.put("内容类型", file.getContentType());
-
-                    String ext = "unknown";
-                    String filename = file.getOriginalFilename(); // 获取上传文件原来的名称
-                    if (filename != null && filename.contains(".")) {
-                        // 获取文件的后缀名
-                        ext = filename.substring(filename.lastIndexOf("."));
-                    }
-                    // 生成服务器使用的文件名称
-                    String newFileName = UUID.randomUUID().toString() + ext;
-                    String filePath = "/home/kiakiana_423/UploadTest/";  // 上传文件保存的路径
-
-                    // 把文件保存到path路径下
-                    File temp = new File(filePath);
-                    if (!temp.exists()) {
-                        temp.mkdirs();
-                    }
-
-                    File localFile = new File(filePath + newFileName);
-                    file.transferTo(localFile);
-
-                    info.put("文件名称", newFileName);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("info: " + info);
-        // 重定向到index页面
-        return "redirect:/index.html";
-    }
-
-//    @PostMapping("/multiUpload")
-//    public Object multiUpload(@RequestParam("file") MultipartFile[] files) {
-//        System.out.println("文件的个数:" + files.length);
-//        for (MultipartFile f : files) {
-//            saveFile(f);
-//        }
-//        return "ok";
-//    }
+//    @PostMapping("/multi-file-upload")
+//    public String multiFileUpload(@RequestParam("up-multi-file") MultipartFile[] files) {
+//        System.out.println("开始处理上传文件");
+//        Map<String, Object> info = new HashMap<>();
 //
-//    private Object saveFile(MultipartFile file) {
-//        if (file.isEmpty()) {
-//            return "未选择文件";
-//        }
-//        String filename = file.getOriginalFilename(); //获取上传文件原来的名称
-//        String filePath = "/home/kiakiana_423/UploadTest";
-//        File temp = new File(filePath);
-//        if (!temp.exists()) {
-//            temp.mkdirs();
-//        }
-//
-//        File localFile = new File(filePath + filename);
 //        try {
-//            file.transferTo(localFile); //把上传的文件保存至本地
-//            System.out.println(file.getOriginalFilename() + " 上传成功");
-//        } catch (IOException e) {
+//            for (MultipartFile file : files) {
+//                if (!file.isEmpty()) {
+//                    info.put("上传文件的参数名称", file.getName());
+//                    info.put("内容类型", file.getContentType());
+//
+//                    String ext = "unknown";
+//                    String filename = file.getOriginalFilename(); // 获取上传文件原来的名称
+//                    if (filename != null && filename.contains(".")) {
+//                        // 获取文件的后缀名
+//                        ext = filename.substring(filename.lastIndexOf("."));
+//                    }
+//                    // 生成服务器使用的文件名称
+//                    String newFileName = UUID.randomUUID().toString() + ext;
+//                    String filePath = "/home/kiakiana_423/UploadTest/";  // 上传文件保存的路径
+//
+//                    // 把文件保存到path路径下
+//                    File temp = new File(filePath);
+//                    if (!temp.exists()) {
+//                        temp.mkdirs();
+//                    }
+//
+//                    File localFile = new File(filePath + newFileName);
+//                    file.transferTo(localFile);
+//
+//                    info.put("文件名称", newFileName);
+//                }
+//            }
+//        } catch (Exception e) {
 //            e.printStackTrace();
-//            return "上传失败";
 //        }
 //
-//        return "ok";
+//        System.out.println("info: " + info);
+//        // 重定向到index页面
+//        return "redirect:/index.html";
 //    }
 
 
